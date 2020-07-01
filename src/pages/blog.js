4ca0c3dcea640f64a25e5e1ce8208b01211cc07d
peters04 <piotreksmolinski04@gmail.com>
2020-07-01 20:24:30 +0200

    add blog.js

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Template from 'templates/Template';
+import PostsGroup from 'components/PostsGroup';
+
+const BlogPage = () => (
+  <Template>
+    <PostsGroup />
+  </Template>
+);
+
+export default BlogPage;
