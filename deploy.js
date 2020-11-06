780701227614327e65149f8250298f28aa8f6761
peters04 <piotreksmolinski04@gmail.com>
2020-11-06 17:07:27 +0100

    add test upload to test dir on ftp (hope this will work)

--- deploy.js
+++ deploy.js
@@ -1,1 +1,25 @@
-console.log('hail the apocalypse');
+const ftp = require('basic-ftp');
+require('dotenv').config();
+
+async function example() {
+  const client = new ftp.Client();
+  client.ftp.verbose = true;
+  try {
+    await client.access({
+      host: process.env.FTP_HOST,
+      user: process.env.FTP_USER,
+      password: process.env.FTP_PASSWD,
+      secure: true,
+      secureOptions: { rejectUnauthorized: false },
+    });
+    console.log(await client.list());
+    await client.ensureDir('/domains/webarts.pl/public_html/test');
+    await client.clearWorkingDir();
+    await client.uploadFromDir(`${__dirname}/build`);
+  } catch (err) {
+    console.log(err);
+  }
+  client.close();
+}
+
+example();
